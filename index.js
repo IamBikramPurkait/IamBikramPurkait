04e67e887bea2e9fd27f8a23b4107ff66a75cf3f
Bikram <bkrmprkt@gmail.com>
2021-04-23 10:31:48 +0530

    update all files

--- index.js
+++ index.js
@@ -92,11 +92,13 @@ const readme = `\
 
 </details>
 
-<details><summary><em>😜 &nbsp;<em>Here's a Joke for you</em></em></summary>
+### <em>🔉 A Famous Fact/Quote</em>
+<!--STARTS_HERE_QUOTE_README-->
+<!--ENDS_HERE_QUOTE_README-->
 
+
+### <em>😜 Here's a Joke for you</em>
 ![Jokes Card](https://readme-jokes.vercel.app/api)
-
-</details>
 
 `
 console.log(readme)
